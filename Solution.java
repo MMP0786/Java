04526d5ea92028b6a8b32f2f4c426bb8c3e79c98
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-02-21 10:18:53 +0530

    gcd and lcm

--- Solution.java
+++ Solution.java
@@ -1,32 +1,62 @@
-import java.io.Console;
-import java.lang.Thread.State;
 import java.util.*;
-public class Solution {
 
-    
+public class Solution {
     public static void main(String[] args){
 
 
        
         Scanner sc= new Scanner(System.in);
-        int n = sc.nextInt();
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
 
-        int c =0;
-        int a =0;
-        int b =1;
-        if(n>1){
+        // int c =0;
+        // int a =0;
+        // int b =1;
+        // if(n>1){
 
-            System.out.println(a+"\n"+b);
+        //     System.out.println(a+"\n"+b);
 
-            for(int x =1; x<=n; x++){
+        //     for(int x =1; x<=n; x++){
 
-                c = a+b;
-                a=b;
-                b=c;
-                System.out.println(c);
+        //         c = a+b;
+        //         a=b;
+        //         b=c;
+        //         System.out.println(c);
+        //     }
+        // }
+
+        // while(n1!= n2){
+        //     System.out.println("hello2");
+        //     if(n1>n2){
+        //         n1 = n1-n2;
+        //     }else{
+        //         n2= n2-n1;
+        //     }
+        // }
+        // System.out.println("Gcd of two no : "+ n2);
+            // if(n2>n1){
+
+            //     for(int a =1; a<=n2; a++ ){
+            //         if(n1%a==0 && n2%a==0){
+            //             System.out.println(a);
+            //         }
+            //     }
+            // }
+
+            // Gcd and Lcm
+
+            int newN1= n1;
+            int newN2= n2;
+            while(n1%n2!=0){
+                int rem = n1%n2;
+                n1= n2;
+                n2 = rem;
+                
+                
             }
-        }
-
+            int lcm = (newN1*newN2)/n2;
+            System.out.println("Gcd of the no : "+ n2);
+            System.out.println("Lcm of the no : "+ lcm);
         
         }
 }
