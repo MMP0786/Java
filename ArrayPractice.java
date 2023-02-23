f69d3dfef9e80b6c49cbf24582907f5194a77b7c
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-02-23 09:48:47 +0530

    max min

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -3,29 +3,36 @@ public class ArrayPractice{
  public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
 
-    int num = sc.nextInt();
+    // int num = sc.nextInt();
 
-    int number [] = new int[num];
-    // number[0]= 12;
-    // number[2]= 22;
-    // number[1]= 32;
+    int number [] = {1002, 24, 36, 34, 442};
 
-    for(int a=0; a<num; a++){
-       number[a] = sc.nextInt();
-    }
+    // int number [] = new int[num];
+    // // number[0]= 12;
+    // // number[2]= 22;
+    // // number[1]= 32;
 
-    // int sum =0;
     // for(int a=0; a<num; a++){
-    //     sum +=number[a];
+    //    number[a] = sc.nextInt();
     // }
-    // System.out.println(sum);
 
-    int search = sc.nextInt();
-
-    for(int a =0; a<num; a++){
-        if(number[a]==search){
-            System.out.println("given no is present at index : "+ a);
+    int min=Integer.MIN_VALUE ; int max =Integer.MAX_VALUE;
+    for(int a=0; a<number.length; a++){
+        if(max>number[a]){
+            max = number[a];
+        }
+        if(min<number[a]){
+            min = number[a];
         }
     }
+    System.out.println("min : "+ min + " And Max : "+ max );
+
+    // int search = sc.nextInt();
+
+    // for(int a =0; a<num; a++){
+    //     if(number[a]==search){
+    //         System.out.println("given no is present at index : "+ a);
+    //     }
+    // }
  }
 }
