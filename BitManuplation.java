30e5f298b219fc24ac95aa49fa717ae1f210dbaf
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-02-28 21:40:10 +0530

    git manupalation get

--- BitManuplation.java
+++ BitManuplation.java
@@ -1,6 +1,19 @@
 public class BitManuplation {
     public static void main(String args[]){
-        
+        int n =6; //1100
+        int pos =1;
+        int bitmask =1<<pos;
+        int notBitMask = ~bitmask;
+
+        // if((bitmask & n) ==0){
+        //     System.out.println(" zero at positon");
+        // }else{
+        //     System.out.println(" one at positon");
+
+        // }
+
+            int newNumber = notBitMask & n;
+            System.out.println(newNumber);
     }
     
 }
