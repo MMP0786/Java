64685ea51079989509100ebeac65dbfc81a740f5
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-03-01 17:53:36 +0530

    clear

--- BitManuplation.java
+++ BitManuplation.java
@@ -1,9 +1,12 @@
 public class BitManuplation {
     public static void main(String args[]){
-        int n =6; //1100
-        int pos =2;
+        int n =5; //0101
+        int pos =0;
         int bitmask =1<<pos;
         int notBitMask = ~bitmask;
+
+        int newNum = notBitMask & n;
+        System.out.println(newNum);
 
         // if((bitmask & n) ==0){
         //     System.out.println(" zero at positon");
@@ -13,8 +16,10 @@ public class BitManuplation {
         // }
 
             // set action
-            int newBit = bitmask | n;
-            System.out.println(newBit);
+            // int newBit = bitmask | n;
+            // System.out.println(newBit);
+
+            // clear action
             int newNumber = notBitMask & n;
             // System.out.println(newNumber);
     }
