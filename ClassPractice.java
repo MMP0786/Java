e16283b3df2f7c5f6dd0ae2f4763e8dc6b8d96ac
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-03-05 21:33:17 +0530

    Classes basic

--- ClassPractice.java
+++ ClassPractice.java
@@ -1,3 +1,31 @@
+class Pen{
+    String color;
+    String type;
+}
+class Animals{
+    String name;
+    String age;
+
+    public void printFn(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+}
 public class ClassPractice {
     
+    public static void main(String args[] ){
+        
+        // System.out.println("hello word");
+        Pen pen1 = new Pen();
+        pen1.color ="black";
+
+        // System.out.println(pen1.color);
+
+        Animals man = new Animals();
+        man.name = "Musharraf";
+        man.age = "14";
+        man.printFn();
+ 
+        System.out.println("hello word");
+    }
 }
--- Recursion.java
+++ Recursion.java
@@ -1,87 +1,87 @@
-public class Recursion {
+// public class Recursion {
 
-// fabacci series
+// // fabacci series
 
-    public static void fabacci(int n, int a, int b){
-        if(n==0){
-            return ;
-        }
-        // System.out.println(a);
-        int c = a+b;
-        System.out.println(a);
-        fabacci(n-1, b, c);
+//     public static void fabacci(int n, int a, int b){
+//         if(n==0){
+//             return ;
+//         }
+//         // System.out.println(a);
+//         int c = a+b;
+//         System.out.println(a);
+//         fabacci(n-1, b, c);
 
-    }
-// With log n complaxcity
+//     }
+// // With log n complaxcity
 
-    public static int PowerFn(int x, int n){
-        if(x==0){
-            return 0;
-        }
-        if(n==0){
-            return 1;
-        }
-    if(n%2==0){
-        return PowerFn(x, n/2) * PowerFn(x, n/2);
-        // return new_x;
-    }else{
-       return PowerFn(x, n/2) * PowerFn(x, n/2)*;
-        // return new_x;
+//     public static int PowerFn(int x, int n){
+//         if(x==0){
+//             return 0;
+//         }
+//         if(n==0){
+//             return 1;
+//         }
+//     if(n%2==0){
+//         return PowerFn(x, n/2) * PowerFn(x, n/2);
+//         // return new_x;
+//     }else{
+//        return PowerFn(x, n/2) * PowerFn(x, n/2)*;
+//         // return new_x;
 
-    }
+//     }
 
-    }
+//     }
 
-//  with O(n) complaxcity
+// //  with O(n) complaxcity
 
-    // public static int PowerFn(int x, int n){
-    //     if(x==0){
-    //         return 0;
-    //     }
-    //     if(n==0){
-    //         return 1;
-    //     }
-    //     // System.out.println(x);
-    //     int new_x = PowerFn(x, n-1);
-    //     new_x = x*new_x;
-    //     return new_x;
+//     // public static int PowerFn(int x, int n){
+//     //     if(x==0){
+//     //         return 0;
+//     //     }
+//     //     if(n==0){
+//     //         return 1;
+//     //     }
+//     //     // System.out.println(x);
+//     //     int new_x = PowerFn(x, n-1);
+//     //     new_x = x*new_x;
+//     //     return new_x;
 
-    // }
+//     // }
 
-    // public static void PrintNo(int n, int fact) {
-    //     if (n == 0) {
-    //         System.out.println(fact);
-    //         return;
-    //     }
-    //     // int new_fact = n
-    //     fact *= n;
-    //     PrintNo(n - 1, fact);
+//     // public static void PrintNo(int n, int fact) {
+//     //     if (n == 0) {
+//     //         System.out.println(fact);
+//     //         return;
+//     //     }
+//     //     // int new_fact = n
+//     //     fact *= n;
+//     //     PrintNo(n - 1, fact);
         
-    // }
+//     // }
 
-    // public static void PrintNo(int i, int sum, int n ){
-    // if(i==n){
-    // sum+=i;
-    // System.out.println(sum);
-    // return;
-    // }
-    // sum+=i;
-    // // System.out.println(sum);
-    // PrintNo(i+1, sum, n);
-    // }
-    // public static void PrintNo(int n){
-    // if(n==6){
-    // return;
-    // }
-    // System.out.println(n);
-    // PrintNo(n+1);
-    // System.out.println(n);
-    // }
-    public static void main(String args[]) {
+//     // public static void PrintNo(int i, int sum, int n ){
+//     // if(i==n){
+//     // sum+=i;
+//     // System.out.println(sum);
+//     // return;
+//     // }
+//     // sum+=i;
+//     // // System.out.println(sum);
+//     // PrintNo(i+1, sum, n);
+//     // }
+//     // public static void PrintNo(int n){
+//     // if(n==6){
+//     // return;
+//     // }
+//     // System.out.println(n);
+//     // PrintNo(n+1);
+//     // System.out.println(n);
+//     // }
+//     public static void main(String args[]) {
 
-        // System.out.println(PowerFn(2, 5));
-      fabacci(7, 0, 1);
-        // System.out.println(outPut);
-        // PrintNo(5, 1);
-    }
-}
+//         // System.out.println(PowerFn(2, 5));
+//     //   fabacci(7, 0, 1);
+//         // System.out.println(outPut);
+//         // PrintNo(5, 1);
+//     }
+// }
