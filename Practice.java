69b73909a92c65010b5febaf7b6aeae5484e4d8a
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-03-11 18:59:51 +0530

    new pattern

--- Practice.java
+++ Practice.java
@@ -1,27 +1,45 @@
 import java.util.*;
 public class Practice {
     public static void primeNo(int a){
-        boolean isPrime = true;
+    //     boolean isPrime = true;
 
-        for(int i=2; i<a/2; a++){
-            if(a%i==0){
-                isPrime = true;
-                break;
-            }
-        }
-        if(isPrime){
-            System.out.println("No is not prime");
+    //     for(int i=2; i<a/2; a++){
+    //         if(a%i==0){
+    //             isPrime = true;
+    //             break;
+    //         }
+    //     }
+    //     if(isPrime){
+    //         System.out.println("No is not prime");
 
-        }else{
-            System.out.println("no is prime");
+    //     }else{
+    //         System.out.println("no is prime");
             
-        }
+    //     }
     }
     public static void main (String args[]){
+        int n=5;
+        int m =4;
 
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        primeNo(a);
+        for(int i =0; i<=5; i++){ // row
+            for(int j=0; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // for(int i =0; i<=5; i++){ // row
+        //     for(int j=0; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // primeNo(a);
 
         // Scanner sc = new Scanner(System.in);
         // int a = sc.nextInt();
