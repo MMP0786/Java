29b18973827bfd46934f7b56a8f5007150cbc7fa
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-03-30 19:47:28 +0530

    binary Search

--- Binary.java
+++ Binary.java
@@ -1,3 +1,26 @@
 public class Binary {
-    
+    public static void main(String[] args) {
+        int arr []= {2, 4, 5, 41, 110};
+        System.out.println(Binary(arr, 2));
+        System.out.println("dlsljdls");
+    }
+    public static int Binary(int arr[], int a){
+      int start =0;
+      int end = arr.length-1;
+
+      while(start<=end){
+        int mid = start +(end-start)/2;
+
+        if(arr[mid]==a){
+            return mid;
+        }
+
+        if(arr[mid]<a){
+            start= mid-1;
+        }else{
+            end = mid+1;
+        }
+      }
+      return -1;
+    }
 }
