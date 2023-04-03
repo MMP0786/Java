5fbc3795761673da7ce389f2007f020cfdffd1d4
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-04-03 11:48:24 +0530

    hollow Butterfly

--- Pattern.java
+++ Pattern.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Pattern{
+public class Pattern {
  public static void main(String[] args){
     // int m=5;
     int n =4;
@@ -27,16 +27,28 @@ public class Pattern{
     // }
 
  
-    for(int i=n; i<=1; i--){
-      for(int j=1; j<=2*(i-1); j++){
-        System.out.print(" *");
-      }
-      for(int j =1; j<=n-i; j++){
-        if(j<=2|| i==1|| i==n|| i/2!=0)
+    for(int i=1; i<=n; i++){
+      for(int j =1; j<=i; j++){
+        if(j==1 || j==i){
         System.out.print("*");
+      }else{
+        System.out.print(" ");
       }
     }
-System.out.println();
-   
+      for(int j=1; j<=2*(n-i); j++){
+        System.out.print(" ");
+      }
+
+      for(int j =1; j<=n; j++){
+        if(j==1 || j==i){
+        System.out.print("*");
+      }else{
+        System.out.print(" ");
+      }
+    }
+    System.out.println();
+
+  }
+  
  }
 }
