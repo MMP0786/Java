e2ffabb28f33765f0d9ec67dbca7eb014b216f29
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-05-30 08:22:22 +0530

    Insertion Sort

--- New folder/sixSorting.java
+++ New folder/sixSorting.java
@@ -18,18 +18,33 @@ public class sixSorting {
 
         // Selecion Sort
 
-        for(int i=0; i<num.length-1; i++ ){
-            int smallest = i;
-            for(int j=i+1; j<num.length; j++){
-                if(num[smallest]> num[j]){
-                    smallest =j;
-                }
-            }
-            int temp = num[smallest];
-            num[smallest]= num[i];
-            num[i]= temp;
+        // for(int i=0; i<num.length-1; i++ ){
+        //     int smallest = i;
+        //     for(int j=i+1; j<num.length; j++){
+        //         if(num[smallest]> num[j]){
+        //             smallest =j;
+        //         }
+        //     }
+        //     int temp = num[smallest];
+        //     num[smallest]= num[i];
+        //     num[i]= temp;
 
+        // }
+
+        // Insertion Sort
+        for(int i=1; i<num.length; i++){
+            int current =num[i];
+            int j =i-1;
+
+            while(j>=0 && current<num[j]){
+                num[j+1] = num[j];
+                j--;
+            }
+            num[j+1] = current;
         }
+            
+
+
         for(int i =0; i<num.length; i++){
             System.out.println(num[i] +" At Index : "+ i);
         }
