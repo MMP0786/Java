001de83a88efb7a49fda8aff8d1d62c76e6a4fd5
Mohd Musarraf <musharrafpanwar@gmail.com>
2023-06-21 07:58:25 +0530

    dfs

--- New folder/Graph.java
+++ New folder/Graph.java
@@ -22,36 +22,36 @@ public class Graph{
         }
 
         graph[0].add(new Edge(0, 1, 10));
-        // graph[0].add(new Edge(0, 2, 10));
+        graph[0].add(new Edge(0, 2, 10));
         
-        graph[1].add(new Edge(1, 0, 0));
-        graph[1].add(new Edge(1, 2, 5));
         // graph[1].add(new Edge(1, 0, 0));
-        // graph[1].add(new Edge(1, 3, 5));
+        // graph[1].add(new Edge(1, 2, 5));
+        graph[1].add(new Edge(1, 0, 0));
+        graph[1].add(new Edge(1, 3, 5));
 
-        graph[2].add(new Edge(2, 1, -5));
-        // graph[2].add(new Edge(2, 0, -5));
-        // graph[2].add(new Edge(2, 4, 12));
-        // graph[2].add(new Edge(2, 3, 2));
+        // graph[2].add(new Edge(2, 1, -5));
+        graph[2].add(new Edge(2, 0, -5));
+        graph[2].add(new Edge(2, 4, 12));
+        graph[2].add(new Edge(2, 3, 2));
 
-        graph[3].add(new Edge(3, 4, 2));
-        graph[4].add(new Edge(4, 3, 2));
-        // graph[3].add(new Edge(3, 1, 2));
         // graph[3].add(new Edge(3, 4, 2));
-        // graph[3].add(new Edge(3, 5, 2));
+        // graph[4].add(new Edge(4, 3, 2));
+        graph[3].add(new Edge(3, 1, 2));
+        graph[3].add(new Edge(3, 4, 2));
+        graph[3].add(new Edge(3, 5, 2));
 // 0-1-2
 // 3-4
 
 
-        // graph[4].add(new Edge(4, 2, 2));
-        // graph[4].add(new Edge(4,3, 2));
-        // graph[4].add(new Edge(4, 5, 2));
+        graph[4].add(new Edge(4, 2, 2));
+        graph[4].add(new Edge(4,3, 2));
+        graph[4].add(new Edge(4, 5, 2));
 
-        // graph[5].add(new Edge(5, 3, 2));
-        // graph[5].add(new Edge(5,4, 2));
-        // graph[5].add(new Edge(5, 6, 2));
+        graph[5].add(new Edge(5, 3, 2));
+        graph[5].add(new Edge(5,4, 2));
+        graph[5].add(new Edge(5, 6, 2));
         
-        // graph[6].add(new Edge(6, 5, 2));
+        graph[6].add(new Edge(6, 5, 2));
 
 
     }
@@ -75,6 +75,18 @@ public class Graph{
         }
 
     }
+
+    // dfs
+    public static void dfs( ArrayList<Edge> graph [], int curr, boolean vis[]){
+        System.out.print(curr+" ");
+        vis[curr]= true;
+
+        for(int i=0; i<graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(vis[e.des]==false)
+                dfs(graph, e.des, vis);
+        }
+    }
     public static void main(String args[]){
 
         int v= 7;
@@ -82,12 +94,23 @@ public class Graph{
         createGraph(graph);
 
         boolean vis[] = new boolean[v];
+        
+        // for(int i=0; i<graph.length; i++){
+        //     if(vis[i]==false){
+        //         bfs(graph, v, vis, i);
+        //     }
+        // }
+        // bfs(graph, v);
+
         for(int i=0; i<graph.length; i++){
             if(vis[i]==false){
-                bfs(graph, v, vis, i);
+                 dfs(graph, i, vis);
             }
         }
-        // bfs(graph, v);
+        
+
+        // dfs
+        
 
 
         // vertex 2 traverse
@@ -97,6 +120,5 @@ public class Graph{
         // }
 
     }
-    private static void bfs(ArrayList<Graph.Edge>[] graph, int v) {
-    }
+    
 }
